16ec9b170fbab06b3c39342a797a63d77da42072
Uğurcan Laçın <ugurcan.lacin@mirado.com>
2022-01-23 22:37:31 +0100

    Initial commit

---
+++ spring-security-authentication/src/main/java/com/backendstory/springsecurityauthentication/SpringSecurityAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.backendstory.springsecurityauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAuthenticationApplication.class, args);
+    }
+    
+}
---
+++ spring-security-authentication/src/test/java/com/backendstory/springsecurityauthentication/SpringSecurityAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstory.springsecurityauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
